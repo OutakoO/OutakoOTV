3d358987d16504bd987b2cbb31e1c65739ee25c7
OutakoO <0u74ko0@gmail.com>
2023-03-06 18:03:27 +0100

    second

--- power/main.js
+++ power/main.js
@@ -1,15 +1,25 @@
 const containerSearch = document.querySelector('section .containerSearch')
 
-const getValue = decodeURI(location.search.substring(1))
 
+if (location.pathname.split('/').includes('search.html')) {
+    const getValue = decodeURI(location.search.substring(1))
+    resultSearch.textContent = getValue
+    search(getValue)
+} else if (location.pathname.split('/').includes('display.html')) {
+    const getValue = decodeURI(location.search.substring(1))
+    aboutAnime(getValue)
+}
+
+
+//get name and img for anime
 async function search(searchValue) {
     const getData = await fetch('https://api.iwannawatch.cf/garson.php?search=' + encodeURIComponent(searchValue))
     const dataJson = await getData.json()
-    console.log(dataJson)
-    for(const item of dataJson){
+    // console.log(dataJson)
+    for (const item of dataJson) {
         const box = document.createElement('a')
         box.classList.add('box')
-        box.setAttribute('href',`display.html?${item.Name}`)
+        box.setAttribute('href', `display.html?${item.Name}`)
         box.style.background = `url(${item.Image})`
         containerSearch.append(box)
     }
@@ -17,8 +27,21 @@ async function search(searchValue) {
 }
 
 
-// console.log(getValue)
-search(getValue)
+//get about anime and episodes
+async function aboutAnime(searchValue) {
+    const getData = await fetch('https://api.iwannawatch.cf/garson.php?Name=' + encodeURIComponent(searchValue))
+    const dataJson = await getData.json()
+    console.log(dataJson)
 
-resultSearch.textContent = getValue
+}
+
+
+if (typeof btnSearch !== 'undefined') {
+    btnSearch.onclick = () => {
+        if (inptSearch.value.length > 0)
+            location.href = `search.html?${inptSearch.value}`
+    }
+}
+
+
 
